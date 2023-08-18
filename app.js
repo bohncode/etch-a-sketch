24e5d3ea0a2576e9c5f9edf46bdf769ed01028b1
bohncode <grantbohn23@gmail.com>
2023-08-18 08:51:41 -0400

    add styles to divs and event listener so that background color will change when mouse enters the div

---
+++ app.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container')
+const sizeOfGrid = 16
+const createGrid = (amtOfGrids) => {
+    for (let i = 0; i < amtOfGrids; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row')
+    for (let j = 0; j < amtOfGrids; j++) {
+        const gridBox = document.createElement('div')
+        gridBox.classList.add('grid-box')
+
+        gridBox.addEventListener('mouseenter', () => {
+            gridBox.style.backgroundColor = 'black'
+        })
+        row.appendChild(gridBox)
+    }
+
+    container.appendChild(row)
+    }
+}
+
+createGrid(sizeOfGrid)
